233e4dba727f52f31b9de2a3915666187ad183b8
nellykelly <niceperson787@gmail.com>
2018-11-15 12:14:28 -0800

    if stamtent
    if $t2 has invalid value, jump to print_invalid_value

--- project2.s
+++ project2.s
@@ -93,11 +93,18 @@ main:
     li $t0, 96
     slt $t1, $t0, $t2
     slti $t4, $t2, 123
-    and $s5, $t1, $t4                           #if $t2 has value within range 97 and 122, $s5 will have 1, else 0
-    addi $s3, $t2, -87                          # $s3 has required value used for calulation later
+    and $s5, $t1, $t4                          
+    addi $s3, $t2, -87                          
     li $t7, 1
-    beq $t7, $s5, calculation                   # if $s5 already has 1, calculate the char's value from ASCII and skip other checks and branch to calculation
-              
+    beq $t7, $s5, calculation                  
+
+    beq $s5, $zero, print_invalid_value         
+
+    calculation:
+    mult $s0, $s3                               
+    mflo $t3
+    add $s1, $s1, $t3                           
+
 
 
              
